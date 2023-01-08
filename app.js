e58d8d019ba999bb2190a15f8aa215ad29cb6044
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-08 14:00:12 -0500

    send different responses

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require("express"); // space to separate 3rd party packages (just styling)
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); // need to call next in order to travel to the next middleware (or else will not see)
-}); // allows us to add a new middleware function (allows request to continue to next middleware in line)
-
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
-  res.send("<h1>Hello from Express</h1>"); // do nott need to set headers ourselves when using Express
-  // ... send response here (note: would not be a next middleware in-line (no default response in express))
+  res.send("<h1>Add Product Page</h1>"); // note how we do not call next (order does matter)
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from Express</h1>"); // do not need to set headers ourselves when using Express
 });
 
 app.listen(3000); // no longer need to import http and create the server like before
