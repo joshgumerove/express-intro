2e91de75ceeb7fcccdfa67c76eac9e4b7a0c33d7
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-08 14:03:20 -0500

    work with middleware

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+  console.log("this will always run");
+  next();
+});
+
 app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
   res.send("<h1>Add Product Page</h1>"); // note how we do not call next (order does matter)
