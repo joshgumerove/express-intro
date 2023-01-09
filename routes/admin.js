05339994beeb18a6f49d578f8709ca8bab928b83
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-09 17:34:14 -0500

    serve html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
-// implicitly /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>' // POST request to product page
-  );
-}); // still need the full path next to the action property
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
 
 router.post("/add-product", (req, res, next) => {
-  console.log("about to redirect");
-  console.log("request body: ", req.body);
-  res.redirect("/"); // note the redirect method added on response by express
+  res.redirect("/");
 });
-// note: same path can be used if methods are different
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html")); // without path core module (would be a file trying to access from OS)
+  // __dirname provides absolute path from OS to specific project folder
 });
 
 module.exports = router;
