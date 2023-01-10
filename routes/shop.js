39fccf40a1a47a9ae5e24cb5e889000d7445c655
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-10 13:19:30 -0500

    create util path function

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const path = require("path");
 
 const express = require("express");
-
 const router = express.Router();
 
+const rootDir = require("../utils/path");
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html")); // without path core module (would be a file trying to access from OS)
+  res.sendFile(path.join(rootDir, "views", "shop.html")); // without path core module (would be a file trying to access from OS)
   // __dirname provides absolute path from OS to specific project folder
 });
 
