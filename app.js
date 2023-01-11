456558e9aff2372f50d20af51500254b136617f2
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-11 10:50:40 -0500

    serve files statically

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const adminRoutes = require("./routes/admin"); // imports router
 const shopRoutes = require("./routes/shop"); // order of the imports does not matter
 
 app.use(bodyParser.urlencoded({ extended: false })); // will not parse all bodies (may at times have to use a different parser)
-
+app.use(express.static(path.join(__dirname, "public"))); // note how we leave out public in html page link (can register multiple static folders)
 app.use("/admin", adminRoutes);
 app.use(shopRoutes); // this order matters
 
